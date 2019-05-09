38bba37adf9b482af5ae48a000112c15ce5041ee
Nick <nick_kinlen@hotmail.com>
2019-05-08 20:22:11 -0400

    Added git ignore file

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb://Nick:Rangers88@ds049094.mlab.com:49094/bookclub',
-  github: {
-    clientID: 'f7a8b88090ef705258df',
-    clientSecret: '0327757af75a520be8484be7577f73d5745189f9'
-  }
-}
